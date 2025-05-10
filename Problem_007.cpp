57eba551d464b2ae4378b0079499d2b497f63387
Md Shariful islam <103664786+Sharif1000@users.noreply.github.com>
2025-05-10 23:34:18 +0600

    Create Problem_007.cpp

---
+++ Problem_007.cpp
@@ -0,0 +1,19 @@
+-- Problem Link: https://codeforces.com/contest/1589/problem/B
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int t; cin>>t;
+    while(t--){
+      int a, b; cin>>a>>b;
+      if((a*b) % 3 == 0) cout<<(a*b) / 3<<endl;
+      else if(a*b == 2) cout<<1<<endl;
+      else{
+        int res = ceil((a * b)*1.0 / 3);
+        cout<<res<<endl;
+      }
+    }
+    return 0;
+}
